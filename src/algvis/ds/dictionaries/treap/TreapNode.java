4faf85a51f8db2b1743a6a682390f640498f164e
Nguyen Duc Huy <huy.duc@eastagile.com>
2016-09-18 17:36:30 +0700

    Update TreapNode.java

--- src/algvis/ds/dictionaries/treap/TreapNode.java
+++ src/algvis/ds/dictionaries/treap/TreapNode.java
@@ -64,16 +64,16 @@ public class TreapNode extends BSTNode {
         final String str = String.valueOf(Math.round(100 * p) / 100.0);
         		
         Color currentViewColor = null;
-		if (!Color.black.equals(v.getColor())) {
-			currentViewColor = v.getColor();
-			v.setColor(Color.black);
-		}
+	if (!Color.black.equals(v.getColor())) {
+	    currentViewColor = v.getColor();
+	    v.setColor(Color.black);
+	}
 		
-		v.drawString(str, x, y - Node.RADIUS * 1.5, Fonts.EXTRA_SMALL);
+	v.drawString(str, x, y - Node.RADIUS * 1.5, Fonts.EXTRA_SMALL);
 
-		if (currentViewColor != null) {
-			v.setColor(currentViewColor);
-		}
+	if (currentViewColor != null) {
+	    v.setColor(currentViewColor);
+	}
     }
 
     void bgPColor() {
